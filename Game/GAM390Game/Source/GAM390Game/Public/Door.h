2a888ef25ed8387e343b6960f32ba84ec966484a
SL295211 <SL295211@falmouth.ac.uk>
2026-01-18 11:33:22 +0000

    door added

---
+++ Game/GAM390Game/Source/GAM390Game/Public/Door.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Door.generated.h"
+
+class USplineComponent;
+class UCameraShakeBase;
+
+
+UCLASS()
+class GAM390GAME_API ADoor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADoor();
+		UFUNCTION(BlueprintCallable)
+    	float const Fall();
+    
+    protected:
+    
+    	UPROPERTY(EditAnywhere)
+    	UStaticMeshComponent* m_Mesh;
+    	
+    	UPROPERTY(EditAnywhere)
+    	USplineComponent* Path;
+    
+    	UPROPERTY(EditAnywhere)
+    	USceneComponent* m_RootComp;
+    
+    	UPROPERTY(EditAnywhere)
+    	float m_Speed;
+    
+    	UFUNCTION()
+    	void MoveMeshTowardsEnd(const float alpha);
+
+};
