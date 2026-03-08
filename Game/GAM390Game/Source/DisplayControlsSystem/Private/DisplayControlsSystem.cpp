8e1cad896fa524815f52a2bff166fae9f7f41d6d
DYLAN GOBEY <DG295303@falmouth.ac.uk>
2026-03-08 22:16:22 +0000

    Implemented the ability to add widgets to rich text

---
+++ Game/GAM390Game/Source/DisplayControlsSystem/Private/DisplayControlsSystem.cpp
@@ -0,0 +1,17 @@
+﻿#include "DisplayControlsSystem.h"
+
+#define LOCTEXT_NAMESPACE "FDisplayControlsSystemModule"
+
+void FDisplayControlsSystemModule::StartupModule()
+{
+    
+}
+
+void FDisplayControlsSystemModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FDisplayControlsSystemModule, DisplayControlsSystem)
