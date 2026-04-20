4e1de49b5e1907bd2b05fcbe315df6628eac2ec7
SAM LLOYD <SL295211@falmouth.ac.uk>
2026-04-20 16:40:56 +0100

    working for grunts

--- Game/GAM390Game/Source/GAM390Game/Private/EnemyMovementComponent.cpp
+++ Game/GAM390Game/Source/GAM390Game/Private/EnemyMovementComponent.cpp
@@ -4,6 +4,7 @@
 #include "EnemyMovementComponent.h"
 
 #include "InteractiveGizmo.h"
+#include "NiagaraVolumeRendererProperties.h"
 
 
 UEnemyMovementComponent::UEnemyMovementComponent(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
@@ -13,6 +14,10 @@ UEnemyMovementComponent::UEnemyMovementComponent(const FObjectInitializer& Objec
 	Deceleration = 2000.f;
 	TurningBoost = 8.f;
 	bPositionCorrected = false;
+	RotationSpeed = 10.f;
+	bIgnoreYaw = false;
+	bIgnorePitch = false;
+	bIgnoreRoll = false;
 	
 	ResetMoveState();
 }
@@ -20,7 +25,7 @@ UEnemyMovementComponent::UEnemyMovementComponent(const FObjectInitializer& Objec
 void UEnemyMovementComponent::BeginPlay()
 {
 	Super::BeginPlay();
-	
+	Mesh = PawnOwner->GetComponentByClass<USkeletalMeshComponent>();
 }
 
 void UEnemyMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType,
@@ -30,47 +35,66 @@ void UEnemyMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType
 	if (ShouldSkipUpdate(DeltaTime)) return;
 	
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-	
+	AController* Controller = GetController();
 	// Make sure that everything is still valid, and that we are allowed to move.
-	if (!PawnOwner || !UpdatedComponent)
+	if (!PawnOwner || !UpdatedComponent || !Controller)
 	{
 		return;
 	}
-	
-	if (IsExceedingMaxSpeed(MaxSpeed) == true)
+	FRootMotionMovementParams RootMotion = Mesh->ConsumeRootMotion();
+	if (RootMotion.bHasRootMotion)
 	{
-		Velocity = Velocity.GetUnsafeNormal() * MaxSpeed;
-	}
-	
-	bPositionCorrected = false;
-	// Move actor
-	FVector Delta = Velocity * DeltaTime;
-	
-	AActor* owner = GetOwner();
-	GetController()->GetDesiredRotation();
-	owner->SetActorRotation(Velocity.GetSafeNormal().Rotation());
-	
-	// Get (and then clear) the movement vector that we set in ACollidingPawn::Tick
-	if (!Delta.IsNearlyZero())
-	{
-		const FVector OldLocation = UpdatedComponent->GetComponentLocation();
-		const FQuat Rotation = UpdatedComponent->GetComponentQuat();
-		
+		FTransform RootMotionTransform = RootMotion.GetRootMotionTransform();
+		FVector Translation = RootMotionTransform.GetTranslation();
+		FRotator Rotation = RootMotionTransform.GetRotation().Rotator();
+
+		// Apply translation and rotation to the actor
 		FHitResult Hit;
-		SafeMoveUpdatedComponent(Delta, Rotation, true, Hit);
- 
-		// If we bumped into something, try to slide along it
-		if (Hit.IsValidBlockingHit())
-		{
-			HandleImpact(Hit, DeltaTime, Delta);
-			SlideAlongSurface(Delta, 1.f - Hit.Time, Hit.Normal, Hit);
-			
-		}
+		MoveUpdatedComponent(Translation, (UpdatedComponent->GetComponentRotation() + Rotation).Quaternion(), true, &Hit, ETeleportType::None);
 		
-		if (!bPositionCorrected)
+	}
+	else
+	{
+		AddInputVector(FVector::DownVector);
+	
+		if (IsExceedingMaxSpeed(MaxSpeed) == true)
 		{
-			const FVector NewLocation = UpdatedComponent->GetComponentLocation();
-			Velocity = ((NewLocation - OldLocation) / DeltaTime);
+			Velocity = Velocity.GetUnsafeNormal() * MaxSpeed;
+		}
+	
+		bPositionCorrected = false;
+		// Move actor
+		FVector Delta = Velocity * DeltaTime;
+	
+		FRotator newRotation = FMath::Lerp<FRotator>(PawnOwner->GetActorRotation(), Controller->GetDesiredRotation(), RotationSpeed * DeltaTime);
+		
+		newRotation.Yaw = bIgnoreYaw ? PawnOwner->GetActorRotation().Yaw : newRotation.Yaw;
+		newRotation.Pitch = bIgnorePitch ? PawnOwner->GetActorRotation().Pitch : newRotation.Pitch;
+		newRotation.Roll = bIgnoreRoll ? PawnOwner->GetActorRotation().Roll : newRotation.Roll;
+		
+		PawnOwner->SetActorRotation(newRotation);
+	
+		// Get (and then clear) the movement vector that we set in ACollidingPawn::Tick
+		if (!Delta.IsNearlyZero())
+		{
+			const FVector OldLocation = UpdatedComponent->GetComponentLocation();
+			const FQuat Rotation = UpdatedComponent->GetComponentQuat();
+		
+			FHitResult Hit;
+			SafeMoveUpdatedComponent(Delta, Rotation, true, Hit);
+ 
+			// If we bumped into something, try to slide along it
+			if (Hit.IsValidBlockingHit())
+			{
+				HandleImpact(Hit, DeltaTime, Delta);
+				SlideAlongSurface(Delta, 1.f - Hit.Time, Hit.Normal, Hit);
+			}
+		
+			if (!bPositionCorrected)
+			{
+				const FVector NewLocation = UpdatedComponent->GetComponentLocation();
+				Velocity = ((NewLocation - OldLocation) / DeltaTime);
+			}
 		}
 	}
 	UpdateComponentVelocity();
--- Game/GAM390Game/Source/GAM390Game/Public/EnemyMovementComponent.h
+++ Game/GAM390Game/Source/GAM390Game/Public/EnemyMovementComponent.h
@@ -28,15 +28,15 @@ private:
 public:
 	
 	/** Maximum velocity magnitude allowed for the controlled Pawn. */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=FloatingPawnMovement)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=EnemyMovement)
 	float MaxSpeed;
 
 	/** Acceleration applied by input (rate of change of velocity) */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=FloatingPawnMovement)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=EnemyMovement)
 	float Acceleration;
 
 	/** Deceleration applied when there is no input (rate of change of velocity) */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=FloatingPawnMovement)
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=EnemyMovement)
 	float Deceleration;
 
 	/**
@@ -44,13 +44,26 @@ public:
 	 * Velocity magnitude is not allowed to increase, that only happens due to normal acceleration. It may decrease with large direction changes.
 	 * Larger values apply extra force to reach the target direction more quickly, while a zero value disables any extra turn force.
 	 */
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=FloatingPawnMovement, meta=(ClampMin="0", UIMin="0"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=EnemyMovement, meta=(ClampMin="0", UIMin="0"))
 	float TurningBoost;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=EnemyMovement)
+	float RotationSpeed;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=EnemyMovement)
+	bool bIgnoreRoll;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=EnemyMovement)
+	bool bIgnoreYaw;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=EnemyMovement)
+	bool bIgnorePitch;
+	
 protected:
 	UPROPERTY(Transient)
 	uint32 bPositionCorrected:1;
 
 	virtual bool
 	ResolvePenetrationImpl(const FVector& Adjustment, const FHitResult& Hit, const FQuat& NewRotation) override;
+	
+	TObjectPtr<USkeletalMeshComponent> Mesh;
 	
 };
