2dd3f3dc39425468992723a74c895f5ea6d1db31
SAM LLOYD <SL295211@falmouth.ac.uk>
2026-05-14 15:38:27 +0100

    FIXED

--- Game/GAM390Game/Source/GAM390Game/Private/CheckPoints/Checkpoint.cpp
+++ Game/GAM390Game/Source/GAM390Game/Private/CheckPoints/Checkpoint.cpp
@@ -1,4 +1,5 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the De
+iption page of Project Settings.
 
 #include "CheckPoints/Checkpoint.h"
 #include "Damageable.h"
